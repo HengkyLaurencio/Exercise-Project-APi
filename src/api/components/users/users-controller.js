66d6adb205dd6867c3a15d2ccabdbdb4cc8adf19
HengkyLaurencio <liu.hengky.laurencio@gmail.com>
2024-04-08 15:51:31 +0700

    add Prevent Duplicate Email in updateUser [Soal 1]

--- src/api/components/users/users-controller.js
+++ src/api/components/users/users-controller.js
@@ -94,6 +94,14 @@ async function updateUser(request, response, next) {
     const name = request.body.name;
     const email = request.body.email;
 
+    const emailIsValid = await usersService.checkEmail(email);
+    if (!emailIsValid) {
+      throw errorResponder(
+        errorTypes.EMAIL_ALREADY_TAKEN,
+        'Email already exist'
+      );
+    }
+
     const success = await usersService.updateUser(id, name, email);
     if (!success) {
       throw errorResponder(
